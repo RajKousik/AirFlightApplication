40288571f5d17dac2685eb37adaff08e587fed00
Kousik Raj <91744323+RajKousik@users.noreply.github.com>
2022-11-16 19:13:13 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,28 +5,32 @@
 
 using namespace std;
 
+//for simplicity, number of flights is considered to be 2
 #define NO_OF_FLIGHTS 2
 
+//a vector array of flight class, which stores the details of the flight and the passengers
 vector<Flight> flights;
 
+//printing the details
 void printDetails()
 {
-    for(Flight &f : flights)
+    for(Flight &f : flights)        //loop thriugh each flight, and print the flight details
     {
-        f.printFlightDetails();
-        f.printPassengerDetails();
+        f.printFlightDetails();     //prints the flight details
+        f.printPassengerDetails();      //prints the passengers details
     }
 }
 
 void bookTickets(Flight* passengerFlight, int passengerID, int noOfTickets, string name, int age)
 {
 
-    string passengerDetails;
+    string passengerDetails;        //stores the information of the passengers
+    
     passengerDetails = "PassengerID = " + to_string(passengerID) + "\nNo. Of Tickets Booked = " + to_string(noOfTickets) + "\n";
 
     passengerDetails += "Name: " + name + "\nAge: " + to_string(age) + "\n";
 
-    passengerFlight->setDetails(passengerDetails, passengerID, noOfTickets);
+    passengerFlight->setDetails(passengerDetails, passengerID, noOfTickets);        //updating the information
 
     passengerFlight->printPassengerDetails();
 }
@@ -34,18 +38,22 @@ void bookTickets(Flight* passengerFlight, int passengerID, int noOfTickets, stri
 
 void getDetails()
 {
+    
     int passengerFlightID;
     cout<<"Enter the Flight ID: ";
-    cin>>passengerFlightID;
+    cin>>passengerFlightID;     //get input about in which flight the user want to book the ticket 
 
+       //if invalid, return error
     if( passengerFlightID <= 0 || passengerFlightID > flights.size())
     {
         cout<<"Invalid Flight ID! \nTry Again!"<<endl<<endl;
         return;
     }
-
+    
+    //creating the object
     Flight* passengerFlight;
-
+    
+    //loop through every flight, and find the user flight by checking the id
     for(Flight &f : flights)
     {
         if(f.getFlightID() == passengerFlightID)
@@ -56,7 +64,8 @@ void getDetails()
     }
 
     passengerFlight->printFlightDetails();
-
+    
+    //if no tickets availabe in the flight, return error
     if(passengerFlight->getTicketsAvailable() == 0)
     {
         cout<<"Oops! No tickets Available at the moment! Try Another Flight!"<<endl<<endl;
@@ -64,39 +73,45 @@ void getDetails()
     }
 
     int noOfTickets;
-
+    
+    //get the number of tickets the user wants to book
     cout<<"Number of Tickets Needed: ";
     cin>>noOfTickets;
 
-
+    
+    //if no of tickets is greater than the available one, return error
     if(noOfTickets > (passengerFlight->getTicketsAvailable()))
     {
 
         cout<<"Sorry! Not enough seats available!"<<endl<<endl;
         return;
     }
-
+    
     /* for getting name with spaces */
     string name;
     cout<<"Enter Your Name: ";
     cin.ignore();
     getline(cin, name);
 
-
+    //get the user age
     int age;
     cout<<"Enter Your Age: ";
     cin>>age;
-
+    
+    //calling the book tickets function
     bookTickets(passengerFlight, passengerFlight->getPassengerID(), noOfTickets, name, age);
-
+    
+    //increment the passenger ID for next passenger
     (passengerFlight->incrementPassengerID());
 
 }
 
 void cancelTickets(Flight* cancelingFlight, int passengerIdToBeCancelled)
 {
+    //calling the cancellingtickets function with the passenger ID to be cancelled
     bool isSuccessful = cancelingFlight->cancellingTickets(passengerIdToBeCancelled);
-
+    
+    //if succesfully cancelled, print the details
     if(isSuccessful)
     {
         cancelingFlight->printFlightDetails();
@@ -107,19 +122,23 @@ void cancelTickets(Flight* cancelingFlight, int passengerIdToBeCancelled)
 
 void getCancelDetails()
 {
+    //the flight ID in which the tickets need to be canceled
+    
     int flightIdToBeCancelled;
     cout<<"Enter the Flight ID : ";
     cin>>flightIdToBeCancelled;
 
+    //if invalid, return error
     if(flightIdToBeCancelled > flights.size())
     {
         cout<<"Invalid Flight ID!"<<endl<<endl;
         return;
     }
 
-
+    //create a object
     Flight* cancellingFlight;
-
+    
+    //find the flight ID
     for(Flight &flight : flights)
     {
         if(flight.getFlightID() == flightIdToBeCancelled)
@@ -128,17 +147,20 @@ void getCancelDetails()
             break;
         }
     }
-
+    
+    //if full tickets are available, then no one booked, so cannot be cancelled
     if(cancellingFlight->getTicketsAvailable() == 50)
     {
         cout<<"No Booking is done so for!..Canceling Operation is failed!"<<endl<<endl;
         return;
     }
-
+    
+    //get the passenger ID to be cancelled
     int passengerIdToBeCancelled;
     cout<<"Enter the passenger ID to be canceled : ";
     cin>>passengerIdToBeCancelled;
-
+    
+    //call the canceltickets function
     cancelTickets(cancellingFlight, passengerIdToBeCancelled);
     return;
 }
@@ -147,41 +169,44 @@ void getCancelDetails()
 int main()
 {
 
-
+    //creating the object for the flight class
     for(int i=0; i<NO_OF_FLIGHTS; i++)
     {
         flights.push_back(Flight());
     }
-
+    
+    //non-stop loop
     while(true)
     {
+        //basic functionalities display
         cout<<"------------WELCOME TO RK AIRLINES------------"<<endl;
         cout<<"1. Book Tickets"<<endl;
         cout<<"2. Cancel Tickets"<<endl;
         cout<<"3. Print Details"<<endl;
         cout<<"4. Exit"<<endl;
         cout<<"Enter Your Choice: "<<endl;
-
+    
         int choice;
 
         cin>>choice;
-
+        
+        //based on the choice, calling the appropriate functions
         switch(choice)
         {
             case 1:
-                getDetails();
+                getDetails();       //for booking tickets, details should be brought from the user
                 break;
             case 2:
-                getCancelDetails();
+                getCancelDetails();     //for cancelling tickets
                 break;
             case 3:
-                printDetails();
+                printDetails();     //printing the details of the user
                 break;
             case 4:
-                cout<<"Thanks for using!!!"<<endl;
-                exit(-1);
+                cout<<"Thanks for using!!!"<<endl;      
+                exit(-1);       //exiting from the function
             default:
-                cout<<"Invalid Choice!!"<<endl;
+                cout<<"Invalid Choice!!"<<endl;     //for invalid input
                 break;
         }
 
